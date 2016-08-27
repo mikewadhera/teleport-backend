2862d1e4d44fe75b9d244a821ac43ffa701df24e
Mike Wadhera <mikewadhera@gmail.com>
2016-08-27 00:37:06 -0700

    New merger

--- lib/merger.rb
+++ lib/merger.rb
@@ -3,52 +3,20 @@ require "tempfile"
 
 class Merger
   
-  SOURCE_FORMAT = "mp4"
-  BITRATE = 6_000_000
+  BITRATE = 8_000_000
+  FORMAT  = "mp4"
   
-  def initialize(first_youtube_url, second_youtube_url)
-    @first_yt = first_youtube_url
-    @second_yt = second_youtube_url
+  def initialize(left, right)
+    @left = left
+    @right = right
   end
   
   def merge!
-    paths = [@first_yt, @second_yt].map do |url|      
-      output = Tempfile.new("merger")
-      
-      path = "#{output.path}.#{SOURCE_FORMAT}"
-      
-      command = "youtube-dl -f bestvideo+bestaudio -o #{path} '#{url}'"
-      
-      #puts command
-      `#{command}`
-      
-      crop_output = Tempfile.new("merger")
-      
-      crop_path = "#{crop_output.path}.#{SOURCE_FORMAT}"
-      
-      crop_command = %{
-        ffmpeg
-          -i #{path}
-          -vf "crop=960:540,setdar=1:1,setsar=1:1" 
-          -s 960X540 
-          -c:v libx264 
-          -qp 0 
-          -preset:v ultrafast 
-          -c:a copy 
-          -c:s copy 
-          -map 0
-          #{crop_path}
-      }.squish
-      
-      #puts crop_command
-      `#{crop_command}`
-
-      crop_path
-    end
+    paths = [@left, @right]
     
     join_output = Tempfile.new("merger")
     
-    join_path = "#{join_output.path}.#{SOURCE_FORMAT}"
+    join_path = "#{join_output.path}.#{FORMAT}"
     
     join_command = %{
       ffmpeg 
@@ -69,18 +37,23 @@ class Merger
     
     box_output = Tempfile.new("merger")
     
-    box_path = "#{box_output.path}.#{SOURCE_FORMAT}"
+    box_path = "#{box_output.path}.#{FORMAT}"
+    
+    if `ffmpeg -codecs| grep nvenc_h264`.empty?
+      encoder = "libx264"
+    else
+      encoder = "nvenc_h264"
+    end
     
     box_command = %{
       ffmpeg 
         -i #{join_path}
-        -vf pad="1920:960:0:210" 
-        -c:v libx264 
-        -b:v #{BITRATE} 
-        -preset:v faster 
-        -tune film 
-        -c:a copy 
-        -c:s copy 
+        -vf pad="1920:960:0:210"
+        -c:v #{encoder}
+        -b:v #{BITRATE}
+        -profile:v baseline
+        -c:a copy
+        -c:s copy
         -map 0 
         #{box_path}
     }.squish
@@ -88,7 +61,7 @@ class Merger
     #puts box_command
     `#{box_command}`
     
-    # Cleanup intermediate files
+    # Cleanup
     `rm -f #{paths[0]} #{paths[1]} #{join_path}`
     
     box_path
