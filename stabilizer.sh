f40e64e414e10ce09ba5330bb09e7170f7bfa200
Mike Wadhera <mikewadhera@gmail.com>
2016-08-26 23:54:51 -0700

    Use tmp dir

--- stabilizer.sh
+++ stabilizer.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
+cd tmp
+dir=`mktemp -d` && cd $dir
+mkdir images
+
+# 1st
 videostab $1 -gpu=yes -ws=yes -r=12 -q --raw
 cd images
 ffmpeg -i %08d.jpg -vcodec libx264 -preset:v ultrafast -qp 0 -c:a copy -c:s copy stable.mp4
 rm *.jpg
 cd ..
+
+# 2nd
 videostab images/stable.mp4 -gpu=yes -ws=yes -r=12 -q --raw
 cd images
 ffmpeg -i %08d.jpg -vcodec libx264 -preset:v ultrafast -qp 0 -c:a copy -c:s copy stable2.mp4
 rm *.jpg
 cd ..
+
+# 3rd
 videostab images/stable2.mp4 -gpu=yes -ws=yes -r=12 -q --raw
-ffmpeg -i %08d.jpg -vcodec libx264 -preset:v ultrafast -qp 0 -c:a copy -c:s copy final.mp4
+cd images
+ffmpeg -i %08d.jpg -vcodec libx264 -preset:v ultrafast -qp 0 -c:a copy -c:s copy $2
+rm *.jpg
+
+# Cleanup
+rm stable.mp4
+rm stable2.mp4
+
+cd .. # images
+cd .. # $dir
+rm -rf $dir
+cd .. # tmp
