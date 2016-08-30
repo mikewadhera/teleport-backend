174165943f54842a99d0d99119f586436fa6fb8d
Mike Wadhera <mikewadhera@gmail.com>
2016-08-29 17:41:04 -0700

    Don't lose audio in stablizer script

--- stabilizer.sh
+++ stabilizer.sh
@@ -7,29 +7,10 @@ cd tmp
 dir=`mktemp -d` && cd $dir
 mkdir images
 
-# 1st
 videostab $1 -gpu=yes -ws=yes -r=12 -q --raw
 cd images
-ffmpeg -i %08d.png -s "$WIDTH"X"$HEIGHT" -vcodec libx264 -preset:v ultrafast -qp 0 -c:a copy -c:s copy $2
+ffmpeg -i %08d.png -i $1 -s "$WIDTH"X"$HEIGHT" -vcodec libx264 -preset:v ultrafast -qp 0 -c:a copy -map 0:v:0 -map 1:a:0 $2
 rm *.png
-#cd ..
-
-# 2nd
-# videostab images/stable.mp4 -gpu=yes -ws=yes -r=12 -q --raw
-# cd images
-# ffmpeg -i %08d.jpg -s "$WIDTH"X"$HEIGHT" -vcodec libx264 -preset:v ultrafast -qp 0 -c:a copy -c:s copy $2 #stable2.mp4
-# rm *.jpg
-#cd ..
-
-# 3rd
-# videostab images/stable2.mp4 -gpu=yes -r=12 -q --raw
-# cd images
-# ffmpeg -i %08d.jpg -s "$WIDTH"X"$HEIGHT" -vcodec libx264 -preset:v ultrafast -qp 0 -c:a copy -c:s copy $2
-# rm *.jpg
-
-# Cleanup
-# rm stable.mp4
-#rm stable2.mp4
 
 cd .. # images
 cd .. # $dir
