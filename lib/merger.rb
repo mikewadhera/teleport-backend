314f559f8a7c3c2b778f81056165ee9d1e25f791
Mike Wadhera <mikewadhera@gmail.com>
2016-09-03 21:10:45 -0700

    Merge without pad

--- lib/merger.rb
+++ lib/merger.rb
@@ -2,8 +2,6 @@
 require "tempfile"
 
 class Merger
-  WIDTH   = 2160
-  HEIGHT  = 1080
   BITRATE = 8_000_000
   FORMAT  = "mp4"
   PROFILE = "baseline"
@@ -22,16 +20,15 @@ class Merger
     
     join_path = "#{join_output.path}.#{FORMAT}"
     
-    # Take two 960X540 and create one 1920x540
-    
     join_command = %{
       ffmpeg 
-        -i #{paths[0]}
-        -i #{paths[1]}
+        -i #{@left}
+        -i #{@right}
         -filter_complex "[0:v:0]pad=iw*2:ih[bg]; [bg][1:v:0]overlay=w" 
-        -c:v libx264 
-        -qp 0 
-        -preset:v ultrafast
+        -c:v #{ENCODER}
+        -b:v #{BITRATE}
+        -preset #{PRESET}
+        -profile:v #{PROFILE}
         -c:a copy 
         -c:s copy 
         #{join_path}
@@ -40,32 +37,7 @@ class Merger
     #puts join_command
     `#{join_command}`
     
-    # Take 1920X540 and letterbox to 2160X1080 (2:1)
-    
-    box_output = Tempfile.new("merger")
-    
-    box_path = "#{box_output.path}.#{FORMAT}"
-    
-    box_command = %{
-      ffmpeg 
-        -i #{join_path}
-        -vf pad="#{WIDTH}:#{HEIGHT}:120:270"
-        -c:v #{ENCODER}
-        -b:v #{BITRATE}
-        -preset #{PRESET}
-        -profile:v #{PROFILE}
-        -c:a copy 
-        -c:s copy 
-        #{box_path}
-    }.squish
-    
-    #puts box_command
-    `#{box_command}`
-    
-    # Cleanup
-    `rm -f #{paths[0]} #{paths[1]} #{join_path}`
-    
-    box_path
+    join_path
   end
   
 end
